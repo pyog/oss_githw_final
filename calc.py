96b81faba26e378f94aa2621797a8ae8d5445ad4
ironmindtiger <ironmindtiger@gmail.com>
2023-04-12 22:08:37 +0900

    3 funcs added

--- calc.py
+++ calc.py
@@ -2,13 +2,19 @@
 # return sum
 def plus(x, y):
     return x+y
+def minus(x, y):
+    return x-y
+def div(x, y):
+    return x/y
+def mul(x, y):
+    return x*y
 
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus")
+        print("0: exit, 1: plus, 2: minus, 3: div, 4: mul")
         check = int(input())
         if check == 1:
             print("First Number")
@@ -16,6 +22,24 @@ def main():
             print("Second Number")
             y = int(input())
             print("answer : ", plus(x,y))
+	  if check == 2:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", minus(x,y))
+	  if check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", div(x,y))
+	  if check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", mul(x,y))
         elif check > 1:
             print("Unsupported")
         else:
